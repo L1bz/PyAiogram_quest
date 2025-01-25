cb21193dfeac965a5168359ab4686732a4a8b253
l1bz <IVANMASK94@yandex.ru>
2025-01-25 12:33:19 +0300

    start

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-
+from aiogram.types import Message
 from config import TOKEN
 
 
 bot = Bot(token= TOKEN)
 dp = Dispatcher()
 
+@dp.message(CommandStart())
+async def cmd_start(message:Message):
+    await message.answer('asdf!')    
+
+
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
+    except RuntimeError:
+        print('exit')
 
